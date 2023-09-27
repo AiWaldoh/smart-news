c2f8d2105820dc354d1f7a3358350a8097d1b499
Waldoh <kevin@gptcoder.pro>
2023-09-27 19:32:37 -0300

    added random colors to topics

--- static/scripts.js
+++ static/scripts.js
@@ -1,55 +1,54 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Initialize with default topics if Local Storage is empty
-    if (!localStorage.getItem('topics')) {
-      localStorage.setItem('topics', JSON.stringify(["World", "Canada"]));
+document.addEventListener("DOMContentLoaded", function () {
+  // Initialize with default topics if Local Storage is empty
+  if (!localStorage.getItem('topics')) {
+    localStorage.setItem('topics', JSON.stringify(["World", "Canada"]));
+  }
+
+  // Load topics from Local Storage and render them
+  const topics = JSON.parse(localStorage.getItem('topics'));
+  renderTopics(topics);
+
+  // Listen for form submissions to add new topics
+  document.getElementById("addTopicForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const newTopic = document.getElementById("newTopic").value;
+    if (newTopic) {
+      addTopic(newTopic);
     }
-  
-    // Load topics from Local Storage and render them
-    const topics = JSON.parse(localStorage.getItem('topics'));
-    renderTopics(topics);
-  
-    // Listen for form submissions to add new topics
-    document.getElementById("addTopicForm").addEventListener("submit", function(e) {
-      e.preventDefault();
-      const newTopic = document.getElementById("newTopic").value;
-      if (newTopic) {
-        addTopic(newTopic);
-      }
-    });
   });
-  
-  // Render topics to the DOM
-  function renderTopics(topics) {
-    const container = document.getElementById("topicsContainer");
-    container.innerHTML = "";  // Clear existing topics
-  
-    topics.forEach((topic, index) => {
-      const topicDiv = document.createElement("div");
-      topicDiv.textContent = topic;
-      
-      // Add delete button
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "Delete";
-      deleteButton.addEventListener("click", () => deleteTopic(index));
-      topicDiv.appendChild(deleteButton);
-  
-      container.appendChild(topicDiv);
-    });
-  }
-  
-  // Add a new topic
-  function addTopic(newTopic) {
-    const topics = JSON.parse(localStorage.getItem('topics'));
-    topics.push(newTopic);
-    localStorage.setItem('topics', JSON.stringify(topics));
-    renderTopics(topics);
-  }
-  
-  // Delete a topic by index
-  function deleteTopic(index) {
-    const topics = JSON.parse(localStorage.getItem('topics'));
-    topics.splice(index, 1);  // Remove the topic at the given index
-    localStorage.setItem('topics', JSON.stringify(topics));
-    renderTopics(topics);
-  }
-  
+});
+
+// Render topics to the DOM
+function renderTopics(topics) {
+  const container = document.getElementById("topicsContainer");
+  container.innerHTML = "";  // Clear existing topics
+
+  topics.forEach((topic, index) => {
+    const topicDiv = document.createElement("div");
+    topicDiv.textContent = topic;
+
+    // Add delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => deleteTopic(index));
+    topicDiv.appendChild(deleteButton);
+
+    container.appendChild(topicDiv);
+  });
+}
+
+// Add a new topic
+function addTopic(newTopic) {
+  const topics = JSON.parse(localStorage.getItem('topics'));
+  topics.push(newTopic);
+  localStorage.setItem('topics', JSON.stringify(topics));
+  renderTopics(topics);
+}
+
+// Delete a topic by index
+function deleteTopic(index) {
+  const topics = JSON.parse(localStorage.getItem('topics'));
+  topics.splice(index, 1);  // Remove the topic at the given index
+  localStorage.setItem('topics', JSON.stringify(topics));
+  renderTopics(topics);
+}
